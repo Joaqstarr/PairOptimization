5105777acbeed3ff424e5fc19bf001b82edffb07
Joaqstarr <73140474+Joaqstarr@users.noreply.github.com>
2026-04-17 23:55:34 -0400

    Added score

---
+++ Assets/Scripts/UI/ScoreUI.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using UnityEngine;
+
+namespace UI
+{
+    public class ScoreUI : MonoBehaviour
+    {
+        private float _score = 0;
+        [SerializeField]
+        private GameObject _player;
+
+        private void Update()
+        {
+            if (_player != null)
+            {
+                _score += Time.deltaTime * 10; // Increase score over time, adjust multiplier as needed
+            }
+            GetComponent<TMPro.TMP_Text>().text = "Score: " + Mathf.FloorToInt(_score);
+        }
+    }
+}
