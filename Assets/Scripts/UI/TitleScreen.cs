219c0306a3432fc1df94a78f9d4ab9b7a9b06a55
Joaqstarr <73140474+Joaqstarr@users.noreply.github.com>
2026-04-17 23:50:25 -0400

    Create title screen

---
+++ Assets/Scripts/UI/TitleScreen.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace UI
+{
+    public class TitleScreen : MonoBehaviour
+    {
+        public void StartGame()
+        {
+            SceneManager.LoadScene("Gameplay");
+        }
+
+        public void QuitGame()
+        {
+            Application.Quit();
+        }
+    }
+}
