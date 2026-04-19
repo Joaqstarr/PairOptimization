dacd8dcfcca8dd0b6d05234f57cfece155cfa62d
SDrastal <sa122032@ucf.edu>
2026-04-19 19:37:44 -0400

    Projectiles for enemies updated

--- Assets/Scripts/Enemies/ProjectileMovement.cs
+++ Assets/Scripts/Enemies/ProjectileMovement.cs
@@ -4,21 +4,24 @@ using UnityEngine;
 
 namespace Enemies
 {
+    //Now checks for gravity at the start for code to run, instead of constantly checking for it in update
+    //Transform is also cached too for optimization
+    [RequireComponent(typeof(GravityMovement))]
     public class ProjectileMovement : MonoBehaviour
     {
         private GravityMovement _gravityMovement;
         [SerializeField] private float _speed;
+        private Transform _cachedTransform;
+
         private void Awake()
         {
             _gravityMovement = GetComponent<GravityMovement>();
+            _cachedTransform = transform;
         }
 
         private void Update()
         {
-            if (_gravityMovement)
-            {
-                _gravityMovement.AddMovementInput(transform.forward * _speed);
-            }
+            _gravityMovement.AddMovementInput(_cachedTransform.forward * _speed);
         }
     }
 }
